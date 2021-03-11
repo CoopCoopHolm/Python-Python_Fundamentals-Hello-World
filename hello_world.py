25a0e1cb2b133380f20d345f8576450aa6ca3498
CoopCoopHolm <78125160+CoopCoopHolm@users.noreply.github.com>
2021-03-10 20:22:49 -0600

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,26 @@
+# print("Hello World!")
+
+# name = "Cooper"
+# print("Hello", name)
+
+# name = "Cooper"
+# print("Hello " + name)
+
+# num = 69
+# print("Hello", num)
+
+# num = 69
+# print("Hello " + str(num))
+
+# food_one = "pizza"
+# food_two = "burgers"
+# print("I love to eat", food_one)
+# print("I love to eat", food_two)
+
+# food_one = "pizza"
+# food_two = "burgers"
+# print(f"I love to eat {food_one} and {food_two}")
+
+fd = "pizza and %s" % "burgers"
+he = "I love to eat"
+print(he, fd)
